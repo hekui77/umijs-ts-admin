923fdb5583d8542f370d11ddb0e0f0c80f1f8d7a
hekui <hekui@tydic.com>
2022-11-30 22:54:46 +0800

    用户列表渲染

--- routes.ts
+++ routes.ts
@@ -1,6 +1,5 @@
 
 export default [
-  { path: '/', component: '@/pages/index' },
   { 
     path: '/login',
     component: '@/pages/login/login',
@@ -12,5 +11,12 @@ export default [
     menuRender: false,
     // 不展示菜单顶栏
     menuHeaderRender: false,
+  },
+  { path: '/', component: '@/pages/index' },
+  { 
+    name: '用户管理',
+    routes: [
+      { name: '用户列表', path: '/userManage/userManage', component: '@/pages/userManage/userManage' }
+    ]
   },
 ]
---
+++ src/pages/userManage/userManage.tsx
@@ -0,0 +1,74 @@
+import type { ColumnsType } from "antd/lib/table";
+import { Table, Button, Space, Popconfirm } from "antd";
+import { useRequest } from "umi"
+import dayjs from 'dayjs';
+
+interface dataType {
+  id: number;
+  nickname: string;
+  phone: string;
+  email: string;
+  createTime: string;
+  updataTime: string;
+}
+const columns: ColumnsType<dataType> = [
+  {
+    title: '昵称',
+    dataIndex: 'nickname',
+  },
+  {
+    title: '电话',
+    dataIndex: 'phone',
+  },
+  {
+    title: '邮箱',
+    dataIndex: 'email',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTime',
+    render: (_, { createTime }) => (
+      <span>{ dayjs(createTime).format('YYYY-MM-DD HH:mm:ss') }</span>
+    )
+  },
+  {
+    title: '更新时间',
+    dataIndex: 'updataTime',
+    render: (_, { updataTime }) => (
+      <span>{ dayjs(updataTime).format('YYYY-MM-DD HH:mm:ss') }</span>
+    )
+  },
+  {
+    title: '操作',
+    dataIndex: '',
+    render: () => (
+      <Space>
+        <Popconfirm
+          title="确定启用该用户？"
+          okText="确定"
+          cancelText=""
+          placement="leftTop"
+        >
+          <Button type="primary" size="small" ghost>启用</Button>
+        </Popconfirm>
+        <Popconfirm
+          title="确定禁用该用户？"
+          okText="确定"
+          cancelText=""
+          placement="leftTop"
+        >
+          <Button size="small" danger>禁用</Button>
+        </Popconfirm>
+      </Space>
+    ),
+  },
+]
+
+export default function UserManage() {
+
+  const { data, loading } = useRequest(() => ({
+    url: '/api/user',
+  }))
+
+  return <Table columns={columns} dataSource={data} loading={loading} rowKey={ (record) => record.id } />
+}
