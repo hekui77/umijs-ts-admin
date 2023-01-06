a5a4ff79030dfa37fd66ec5a2fc9989bc05ebdaa
hekui <1287255589@qq.com>
2023-01-06 23:42:18 +0800

    通用布局

--- routes.ts
+++ routes.ts
@@ -7,14 +7,20 @@ export default [
     path: '/register',
     component: '@/pages/register/register',
   },
-  { path: '/', component: '@/pages/index' },
   {
-    name: '用户管理',
+    path: '/',
+    component: '@/layouts/index',
     routes: [
+      { path: '/', component: '@/pages/index' },
       {
-        name: '用户列表',
-        path: '/userManage/userManage',
-        component: '@/pages/userManage/userManage',
+        name: '用户管理',
+        routes: [
+          {
+            name: '用户列表',
+            path: '/userManage/userManage',
+            component: '@/pages/userManage/userManage',
+          },
+        ],
       },
     ],
   },
---
+++ src/layouts/index.tsx
@@ -0,0 +1,75 @@
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { useState } from 'react';
+const { Header, Content, Footer, Sider } = Layout;
+import type { MenuProps } from 'antd';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+type MenuItem = Required<MenuProps>['items'][number];
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+const items: MenuItem[] = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [
+    getItem('Team 1', '6'),
+    getItem('Team 2', '8'),
+  ]),
+  getItem('Files', '9', <FileOutlined />),
+];
+
+export default (props: any) => {
+  // return <div style={{ padding: 20 }}>{ props.children }</div>;
+
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <div
+          style={{
+            height: 32,
+            margin: 16,
+            background: 'rgba(255, 255, 255, 0.2)',
+          }}
+        />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={['1']}
+          mode="inline"
+          items={items}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: 16, background: '#fff' }}>
+          <div style={{ padding: 16 }}>{props.children}</div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
